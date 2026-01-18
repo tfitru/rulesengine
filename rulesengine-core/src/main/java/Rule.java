6007a093df6c729b4b5bca02bd4c406b92f16643
tfitru <tfitru@github.com>
2026-01-18 14:24:46 -0500

    Adding rules and class to process

---
+++ rulesengine-core/src/main/java/Rule.java
@@ -0,0 +1,9 @@
+import lombok.Data;
+
+@Data
+public class Rule {
+    private String rule;
+    private int credit_score;
+    private int credit_history;
+    private String status;
+}
--- rulesengine-core/src/main/java/RulesEngineMain.java
+++ rulesengine-core/src/main/java/RulesEngineMain.java
@@ -1,32 +1,11 @@
+import tools.jackson.databind.ObjectMapper;
+
+
 void main() {
 
-    // The project references the rules
-    // and the engine produces a decisions
-
-    // A Fact comes in could be anything
-    // Credit Card Application
-    //      Example:
-    //              Credit Score: 680
-    //              Credit History: 1 year
-    //
-
-    // The rules
-    // Have the condition and decision
-    //      Example:
-    //      Rule - 1:
-    //          IF credit score is >= 600
-    //          AND credit history is >= 1 year
-    //          THEN declare "Approved"
-    //
-    //      Rule - 2:
-    //          IF credit history is < 1 year
-    //          THEN declare "not approved"
-
-
-    // Rules Engine
-    //      Example:
-    //          Based on the Rules the output
-    //          is from the decision nothing else
+    ObjectMapper mapper = new ObjectMapper();
+    // Grab the rules from json file
+    Processer processer = mapper.readValue(new File("rulesengine-core/src/main/resources/rules.json"), Processer.class);
 
 
 
